4ebc98587bcb5b2fb7d5c7ff023389c59d23a5ce
Anastassiya <yamorzhinka@gmail.com>
2020-09-10 15:29:20 +0300

    Redirect to Profile

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Redirect, Route} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
+
 
 const App = (props) => {
 
@@ -20,6 +21,9 @@ const App = (props) => {
         <Navbar/>
         <div className="app-wrapper-content">
 
+          <Route exact path='/'>
+            <Redirect to='/profile'/>
+          </Route>
           <Route path='/dialogs' render={ DialogsRoute }/>
           <Route path='/profile' render={ () => <Profile postMessages={props.postMessages}/>}/>
           <Route path='/news' render={ () => <News/>}/>
