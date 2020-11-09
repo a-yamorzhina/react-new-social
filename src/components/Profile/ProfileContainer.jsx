bbc3fa06cc32a95a4344fb8f4e0919dbf2442fe6
Anastassiya <yamorzhinka@gmail.com>
2020-11-09 14:48:00 +0300

    60 withRouter

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -8,13 +8,11 @@ import {withRouter} from "react-router";
 
 class ProfileContainer extends React.Component {
 
-
   componentDidMount() {
     let userId = this.props.match.params.userId;
     if (!userId) {
       userId = 2;
     }
-
     axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
       .then(response => {
         // console.log(response);
