a19b71c9be51f0ba9e4e00e8658ca5ceaa2786a5
Anastassiya <yamorzhinka@gmail.com>
2020-11-10 21:21:05 +0300

    61 with credentials, cookie

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import allUsersReducer from "./allUsers-reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   sidebar: sidebarReducer,
-  allUsersPage: allUsersReducer
+  allUsersPage: allUsersReducer,
+  auth: authReducer
 });
 
 let store = createStore(reducers);
