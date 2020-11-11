43bc447d48a5a97d7195c6c34721d45a25afcb32
Anastassiya <yamorzhinka@gmail.com>
2020-11-11 11:07:46 +0300

    62 follow/unfollow API

--- src/components/AllUsers/AllUsersContainer.jsx
+++ src/components/AllUsers/AllUsersContainer.jsx
@@ -22,7 +22,7 @@ import Preloader from "../common/Preloader/Preloader";
 
     if (this.props.users.length === 0) {
       this.props.toggleIsFetching(true);
-      axios.get(`https://social-network.samuraijs.com/api/1.0/users/?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users/?page=${this.props.currentPage}&count=${this.props.pageSize}`, {withCredentials: true})
         .then(response => {
           // console.log(response);
           this.props.toggleIsFetching(false);
@@ -35,7 +35,7 @@ import Preloader from "../common/Preloader/Preloader";
   onPageChanged = (pageNumber) => {
     this.props.setCurrentPage(pageNumber);
     this.props.toggleIsFetching(true);
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users/?page=${pageNumber}&count=${this.props.pageSize}`)
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users/?page=${pageNumber}&count=${this.props.pageSize}`, {withCredentials: true})
       .then(response => {
         this.props.toggleIsFetching(false);
         this.props.setUsers(response.data.items);
--- src/components/AllUsers/User/User.jsx
+++ src/components/AllUsers/User/User.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "../AllUsers.module.css";
 import {NavLink} from "react-router-dom";
+import * as axios from "axios";
 
 
 const User = (props) => {
@@ -30,19 +31,47 @@ const User = (props) => {
             {props.status}
           </p>
           {/*<ul className={s.connections}>*/}
-            {/*<li>*/}
-              {/*<span className={s.count}>{props.friends}</span>*/}
-              {/*<p className={s.mute}>Friends</p>*/}
-            {/*</li>*/}
-            {/*<li>*/}
-              {/*<span className={s.count}>{props.groups}</span>*/}
-              {/*<p className={s.mute}>Groups</p>*/}
-            {/*</li>*/}
+          {/*<li>*/}
+          {/*<span className={s.count}>{props.friends}</span>*/}
+          {/*<p className={s.mute}>Friends</p>*/}
+          {/*</li>*/}
+          {/*<li>*/}
+          {/*<span className={s.count}>{props.groups}</span>*/}
+          {/*<p className={s.mute}>Groups</p>*/}
+          {/*</li>*/}
           {/*</ul>*/}
         </div>
         {props.followOrNot
-          ? <button className={s.secondButton} onClick={() => {changeToUnfollow(props.id)} }>Unfollow</button>
-          : <button className={s.secondButton} onClick={ () => {changeToFollow(props.id)} }>Follow</button>}
+          ? <button className={s.secondButton} onClick={() => {
+            axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${props.id}`, {
+              withCredentials: true,
+              headers: {
+                "API-KEY": "88b6ce23-82c2-4b75-8595-b3c7a16fb38a"
+              }
+            })
+              .then(response => {
+                if (response.data.resultCode === 0) {
+                  changeToUnfollow(props.id);
+                }
+              });
+          }}>
+            Unfollow
+          </button>
+          : <button className={s.secondButton} onClick={() => {
+            axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${props.id}`, {}, {
+              withCredentials: true,
+              headers: {
+                "API-KEY": "88b6ce23-82c2-4b75-8595-b3c7a16fb38a"
+              }
+            },)
+              .then(response => {
+                if (response.data.resultCode === 0) {
+                  changeToFollow(props.id);
+                }
+              })
+          }}>
+            Follow
+          </button>}
 
       </div>
     </div>
