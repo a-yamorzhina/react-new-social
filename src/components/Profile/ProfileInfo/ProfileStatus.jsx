04410b3a606ad0c34d5329986ddf43f4970e78c9
Anastassiya <yamorzhinka@gmail.com>
2020-11-30 15:32:18 +0300

    74 componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -28,6 +28,14 @@ class ProfileStatus extends React.Component {
     })
   }
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.status !== this.props.status) {
+      this.setState({
+        status: this.props.status
+      })
+    }
+  }
+
   render() {
     return (
       <div className={s.statusContainer}>
