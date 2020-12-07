f114e7f6c469c88684b75acb28f6c593a0562c21
Anastassiya <yamorzhinka@gmail.com>
2020-12-07 18:06:35 +0300

    77 field validation, errors

--- src/components/Login/Form/LoginForm.jsx
+++ src/components/Login/Form/LoginForm.jsx
@@ -3,6 +3,8 @@ import s from "../Login.module.css";
 import miniUser from "../../../assets/images/mini-user.svg"
 import key from "../../../assets/images/key.svg"
 import {Field, reduxForm} from "redux-form";
+import {required} from "../../../utils/validators/validators";
+import {Input} from "../../common/FormsControls/FormsControls";
 
 const LoginForm = (props) => {
   return (
@@ -12,14 +14,14 @@ const LoginForm = (props) => {
             <span className={s.icon}>
             <img className={s.userIcon} src={miniUser} alt="mini-user"/>
           </span>
-            <Field component={"input"} name={"login"} className={s.input} placeholder='Email or username' type='text'/>
+            <Field component={Input} validate={[required]} name={"login"} className={s.input} placeholder='Email or username' type='text'/>
           </div>
           <div className={s.password}>
             <label></label>
             <span className={s.iconPassword}>
             <img className={s.passwordIcon} src={key} alt="password"/>
           </span>
-            <Field component={"input"} name={"password"} className={s.input} placeholder='Password' type="password"/>
+            <Field component={Input} name={"password"} validate={[required]} className={s.input} placeholder='Password' type="password"/>
           </div>
           <div className={s.rememberMe}>
             <label className={s.rememberText}>
---
+++ src/components/common/FormsControls/FormsControls.js
@@ -0,0 +1,31 @@
+import React from "react";
+import s from "./FormsConttrols.module.css"
+
+const FormControl = ({input, meta, child, ...props}) => {
+
+  const hasError = meta.touched && meta.error;
+
+  return (
+    <div className={s.formControl + " " + (hasError && s.error)}>
+      {props.children}
+    </div>
+  )
+};
+
+export const Textarea = (props) => {
+  const {input, meta, child, ...restProps} = props;
+  const hasError = meta.touched && meta.error;
+  return (
+    <FormControl {...props}>
+      <textarea {...input} {...restProps}/>
+      {hasError && <span>{meta.error}</span>}
+    </FormControl>
+  )
+};
+
+export const Input = (props) => {
+  const {input, meta, child, ...restProps} = props;
+  return (
+    <FormControl {...props}> <input {...input} {...restProps}/> </FormControl>
+  )
+};
