29b7d2a35e4a26382e07aa3c59703eefe4084583
Anastassiya <yamorzhinka@gmail.com>
2021-01-22 12:35:34 +0300

    95 gh pages success

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -21,6 +21,7 @@ let reducers = combineReducers({
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 // window.__store__ = store;
+// gh pages - не воспринимает composeEnhancer
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
