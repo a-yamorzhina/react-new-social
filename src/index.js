9c79f6c219c235190f558eb56b70dbc16be87c6a
Anastassiya <yamorzhinka@gmail.com>
2021-01-22 12:34:54 +0300

    95 gh pages success

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import store from "./redux/redux-store";
 import ReactDOM from "react-dom";
 import App from "./App";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <Provider store={store}>
       <App/>
     </Provider>
-  </BrowserRouter>, document.getElementById("root")
+  </HashRouter>, document.getElementById("root")
 );
 
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
@@ -18,11 +18,11 @@ let reducers = combineReducers({
   app: appReducer,
 });
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
-window.__store__ = store;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+// window.__store__ = store;
 
-// let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 window.store = store;
 
